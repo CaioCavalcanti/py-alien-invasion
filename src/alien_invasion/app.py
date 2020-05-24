fae6bfdc7ede3109b12c027a3c33719d80e79ea5
Caio F. M. Cavalcanti <caiofabiomc@gmail.com>
2020-05-24 16:58:02 +0200

    adds control to stop game when reach ship limit

--- src/alien_invasion/app.py
+++ src/alien_invasion/app.py
@@ -31,9 +31,10 @@ class AlienInvasion:
         """Start the main loop for the game"""
         while True:
             self._check_events()
-            self.ship.update()
-            self._update_bullets()
-            self._update_fleet()
+            if self.stats.game_active:
+                self.ship.update()
+                self._update_bullets()
+                self._update_fleet()
             self._update_screen()
 
     def _check_events(self):
@@ -96,15 +97,18 @@ class AlienInvasion:
 
     def _hit_ship(self):
         """Respond to the ship being hit by an alien"""
-        self.stats.ships_left -= 1
-        # Get rid of any remaining aliens and bullets
-        self.bullets.empty()
-        self.aliens.empty()
-        # Create a new fleet and center the ship
-        self._create_fleet()
-        self.ship.center()
-        # Pause
-        sleep(0.5)
+        if self.stats.ships_left > 0:
+            self.stats.ships_left -= 1
+            # Get rid of any remaining aliens and bullets
+            self.bullets.empty()
+            self.aliens.empty()
+            # Create a new fleet and center the ship
+            self._create_fleet()
+            self.ship.center()
+            # Pause
+            sleep(0.5)
+        else:
+            self.stats.game_active = False
 
     def _check_aliens_hit_bottom(self):
         """Checks if any alien hit the bottom of the screen"""
